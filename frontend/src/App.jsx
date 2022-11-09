25b106bff8751877933e6ba14f4caab3beec5c4a
Orenikoe <orental21@gmail.com>
2022-11-09 09:57:59 +0000

    app page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-
 import Form from "./components/Form/Form";
-
 import ScrollUpBtn from "./components/ScrollUpBtn/ScrollUpBtn";
-
 import NavBar from "./components/NavBar/NavBar";
-import Sport from "./components/Sport";
 import Footer from "./components/Footer/Footer";
 import Subscribe from "./components/Subscribe/Subscribe";
 import EventList from "./components/EventList/EventList";
-
-import Register from "./components/Form/FormSignUp";
-
 import { EventsContextProvider } from "./contexts/EventsContext";
 import EventDetails from "./components/EventDetails/EventDetails";
 
@@ -21,25 +14,15 @@ function App() {
   return (
     <>
       <NavBar />
-      {/* <Form /> */}
-      {/* <Routes>
-        <Route path="/Sport" element={<Sport />} />
-      </Routes> */}
-
-      <Routes>
-        <Route path="/components/Register/Register" element={<Register />} />
-      </Routes>
-
       <EventsContextProvider>
         <Routes>
           <Route path="/events/:id" element={<EventDetails />} />
           <Route path="/" element={<EventList />} />
+          <Route path="/login" element={<Form />} />
         </Routes>
       </EventsContextProvider>
-
       <Subscribe />
       <Footer />
-
       <ScrollUpBtn />
     </>
   );
