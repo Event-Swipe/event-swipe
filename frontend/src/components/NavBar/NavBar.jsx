8ed422564e0ceef1da1fac3aa75bc6e53caaf4f8
r8ch <81969279+r8ch@users.noreply.github.com>
2022-11-21 12:35:55 +0000

    create the branch and fix navbar style

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@
 import React, { useState, useContext } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
 import "./NavBar.css";
-import { BiMenu } from "react-icons/bi";
+// import { BiMenu } from "react-icons/bi";
 import SearchBar from "../SearchBar/SearchBar";
 import EventsContext from "../../contexts/EventsContext";
 import UserContext from "../../contexts/UserContext";
@@ -22,48 +22,77 @@ function NavBar() {
   };
 
   return (
-    <nav className="navbar">
-      <NavLink to="/" onClick={clearSearch}>
-        <img
-          src="https://i.postimg.cc/XYtHc1Cq/company-logo-no-Bg.png"
-          className="company-logo"
-          alt="company-logo"
-        />
-      </NavLink>
-      <SearchBar />
-      <ul className="links" id={showLinks ? "hidden" : "none"}>
-        <NavLink onClick={clearSearch} to="/events/sport" className="current">
-          <li>Sports</li>
+    <nav className="navbar navbar-expand-md navbar-dark  bg-dark">
+      <div className="container-fluid">
+        <NavLink to="/" onClick={clearSearch} className="navbar-brand">
+          <img
+            src="https://i.postimg.cc/XYtHc1Cq/company-logo-no-Bg.png"
+            className="company-logo"
+            alt="company-logo"
+          />
         </NavLink>
-        <NavLink
-          onClick={clearSearch}
-          to="/events/concerts"
-          className="current"
-        >
-          <li>Concert</li>
-        </NavLink>
-        <NavLink onClick={clearSearch} to="events/theater" className="current">
-          <li>Theater</li>
-        </NavLink>
-        {userDetails !== null ? (
-          <i onClick={() => navigate("/dashboard")} className="pi pi-user" />
-        ) : (
-          <NavLink
-            onClick={clearSearch}
-            to="/login"
-            className="currentLoginLink"
-          >
-            <li>Log in</li>
-          </NavLink>
-        )}
-      </ul>
-      <div className="button-div">
         <button
-          className="burger-menu-btn"
-          onClick={() => setShowLinks(!showLinks)}
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <BiMenu className="menu" />
+          <span className="navbar-toggler-icon" />
         </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <SearchBar />
+          <ul
+            className="nav-list navbar-nav ms-auto me-2 mb-2 mb-md-0"
+            id={showLinks ? "hidden" : "none"}
+          >
+            <NavLink
+              onClick={clearSearch}
+              to="/events/sport"
+              className="current nav-link active"
+            >
+              <li className="nav-item">Sports</li>
+            </NavLink>
+            <NavLink
+              onClick={clearSearch}
+              to="/events/concerts"
+              className="current nav-link active"
+            >
+              <li className="nav-item">Concert</li>
+            </NavLink>
+            <NavLink
+              onClick={clearSearch}
+              to="events/theater"
+              className="current nav-link active"
+            >
+              <li className="nav-item">Theater</li>
+            </NavLink>
+            {userDetails !== null ? (
+              <i
+                onClick={() => navigate("/dashboard")}
+                className="pi pi-user"
+              />
+            ) : (
+              <NavLink
+                onClick={clearSearch}
+                to="/login"
+                className="currentLoginLink nav-link active"
+              >
+                <li className="nav-item">Log in</li>
+              </NavLink>
+            )}
+          </ul>
+        </div>
+        {/* <div className="button-div">
+          <button
+            className="burger-menu-btn"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <BiMenu className="menu" />
+          </button>
+        </div> */}
       </div>
     </nav>
   );
