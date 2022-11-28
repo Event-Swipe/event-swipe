5416fe547ced03b1b5be029ca319718757a080af
Orenikoe <orental21@gmail.com>
2022-11-28 22:52:52 +0000

    completed

--- backend/src/models/registrationHandler.js
+++ backend/src/models/registrationHandler.js
@@ -1,9 +1,14 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable object-shorthand */
 /* eslint-disable prettier/prettier */
 /* eslint-disable prefer-template */
 /* eslint-disable prettier/prettier */
+const jwt = require('jsonwebtoken');
 const connection = require("../db");
 
-const getLoginCredentials = (req, res) => {
+
+const getLoginCredentials = (req, res, next) => {
+
   const { email, password } = req.params;
 
   connection
@@ -11,14 +16,27 @@ const getLoginCredentials = (req, res) => {
       `SELECT * FROM users where email = '${email}' and password = '${password}' `
     )
     .then(([result]) => {
-      res.send(result[0]);
+      req.user=result[0];
+      next()
     })
     .catch((err) => {
       console.error(err + "Problem");
       res.status(500).send("Error adding the user");
     });
 };
+const generateToken = (req, res) => {
+  const token = jwt.sign({ password: req.user.password, email: req.user.email }, 'lalala')
+
+    // .then((res) => {
+     res.status(200).send({user: req.user, token: token})
+    // })
+    // .catch((err) => {
+    //   console.error(err + "Problem");
+    //   res.status(500).send("Error adding the user");
+    // });
+};
 
 module.exports = {
   getLoginCredentials,
+  generateToken
 };
--- frontend/src/contexts/UserContext.jsx
+++ frontend/src/contexts/UserContext.jsx
@@ -1,19 +1,25 @@
+/* eslint-disable camelcase */
 /* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable react/prop-types */
 import { useState, createContext } from "react";
+import jwt_decode from "jwt-decode";
 
 const UserContext = createContext();
 export default UserContext;
 export function UserContextProvider({ children }) {
   // creating the states for the variables
-  const [userDetails, setUserDetails] = useState(null);
+  const [userDetails, setUserDetails] = useState(
+    localStorage.getItem("key") === null
+      ? null
+      : jwt_decode(localStorage.getItem("key"))
+  );
   const [userNotFound, setUserNotFound] = useState(false);
-
   const LoginFunction = async (email, password) => {
     await fetch(`http://localhost:5000/login/${email}/${password}`)
       .then((response) => response.json())
       .then((data) => {
         setUserDetails(data);
+        localStorage.setItem("key", data.token);
       })
       .catch((error) => {
         setUserNotFound(true);
